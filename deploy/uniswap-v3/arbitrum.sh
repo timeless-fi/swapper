35db08558aa01e8473ada13c092e24857d01461b
ZeframLou <zeframlou@gmail.com>
2022-08-22 17:51:47 -0700

    build: deploy to arbitrum

---
+++ deploy/uniswap-v3/arbitrum.sh
@@ -0,0 +1,19 @@
+# load environment variables from .env
+source .env
+
+# set env variables
+export ADDRESSES_FILE=./deployments/arbitrum.json
+export RPC_URL=$RPC_URL_ARBITRUM
+
+# load common utilities
+. $(dirname $0)/../common.sh
+
+# deploy contracts
+univ3_swapper_address=$(deploy UniswapV3Swapper 0x000000000000000000000000000000000000dEaD $WETH_ARBITRUM $PROTOCOL_FEE_ARBITRUM $UNIV3_FACTORY)
+echo "UniswapV3Swapper=$univ3_swapper_address"
+
+send $univ3_swapper_address "transferOwnership(address,bool,bool)" $INITIAL_OWNER_ARBITRUM true false
+echo "UniswapV3SwapperOwner=$INITIAL_OWNER_ARBITRUM"
+
+univ3_juggler_address=$(deploy UniswapV3Juggler $UNIV3_FACTORY $UNIV3_QUOTER)
+echo "UniswapV3Juggler=$univ3_juggler_address"
